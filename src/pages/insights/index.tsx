01a90851ea40511dc2c4210dce6147a4210fe868
jonathan1965 <jonakarisa@gmail.com>
2025-02-08 17:44:17 +0200

    make it better

--- src/pages/insights/index.tsx
+++ src/pages/insights/index.tsx
@@ -38,24 +38,25 @@ const Insights: NextPage = () => {
 
           <div className="bg-white p-4 md:p-8 cursor-default flex flex-col items-center justify-center w-full">
             <div className="w-full md:max-w-6xl">
-              <h1 className="text-primary italic flex w-full items-center space-x-3 text-lg md:text-xl font-semibold">
+              {/* <h1 className="text-primary italic flex w-full items-center space-x-3 text-lg md:text-xl font-semibold">
                 <span>Recent.</span>{' '}
                 <span className="h-0.5 opacity-25 w-64 md:w-92 bg-primary" />
-              </h1>
+              </h1> */}
               <div className="mt-3 grid sm:grid-cols-2 gap-3 md:grid-cols-4">
                 <InsightItem />
               </div>
              
-
+{/* 
               <h1 className="mt-12 text-primary italic flex w-full items-center space-x-3 text-lg md:text-xl font-semibold">
                 <span>Case summaries.</span>{' '}
                 <span className="h-0.5 opacity-25 w-64 md:w-92 bg-primary" />
               </h1>
 
-<h1 className="mt-12 text-primary italic flex w-full items-center space-x-3 text-lg md:text-xl font-semibold">
-  <span>Others.</span>
-  <span className="h-0.5 opacity-25 w-64 md:w-92 bg-primary" />
-</h1>
+              <h1 className="mt-12 text-primary italic flex w-full items-center space-x-3 text-lg md:text-xl font-semibold">
+                <span>Others.</span>
+                <span className="h-0.5 opacity-25 w-64 md:w-92 bg-primary" />
+              </h1> */}
+
             </div>
           </div>
         </div>
