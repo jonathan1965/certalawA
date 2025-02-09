87a1a3527723e0b4fbf24e496b87fdb952aff8c0
jonathan1965 <jonakarisa@gmail.com>
2025-02-09 11:42:07 +0200

    make it better

--- next.config.js
+++ next.config.js
@@ -1,24 +1,53 @@
 /** @type {import('next').NextConfig} */
+// module.exports = {
+//   output: 'export',
+//   distDir: 'dist',
+//   swcMinify: true,
+//   trailingSlash: true,
+//   skipTrailingSlashRedirect: true,
+//   images: {
+//     unoptimized: true,
+//   },
+//   reactStrictMode: true,
+//   typescript: {
+//     ignoreBuildErrors: true,
+//   },
+//   rewrites: async () => {
+//     return [
+//       {
+//         source: '/',
+//         destination: '/index.html',
+//       },
+//     ];
+//   },
+//   typescript: {
+//     // !! WARN !!
+//     // Dangerously allow production builds to successfully complete even if
+//     // your project has type errors.
+//     // !! WARN !!
+//     ignoreBuildErrors: true,
+//   },
+//   eslint: {
+//     // Warning: This allows production builds to successfully complete even if
+//     // your project has ESLint errors.
+//     ignoreDuringBuilds: true,
+//   },
+// };
 module.exports = {
-  reactStrictMode: true,
-  rewrites: async () => {
-    return [
-      {
-        source: '/',
-        destination: '/index.html',
-      },
-    ]
-},
+  reactStrictMode: false,
   typescript: {
-    // !! WARN !!
-    // Dangerously allow production builds to successfully complete even if
-    // your project has type errors.
-    // !! WARN !!
     ignoreBuildErrors: true,
   },
+  // swcMinify: true,
+  output: 'export',
+  trailingSlash: true,
+  skipTrailingSlashRedirect: true,
+  distDir: 'dist',
+  images: {
+    unoptimized: true,
+  },
   eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
     ignoreDuringBuilds: true,
   },
-}
+};
+
